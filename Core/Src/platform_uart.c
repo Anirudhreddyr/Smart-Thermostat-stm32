2165def3756e63c7b6bb810f028b1f9f6dd351e6
Anirudh Reddy R <anirudhreddy1710@gmail.com>
2026-03-16 02:51:14 +0530

    Add platform_uart_init and platform_uart_send functions
    Implement UART initialization and message sending functions.

--- Core/Src/platform_uart.c
+++ Core/Src/platform_uart.c
@@ -1,1 +1,28 @@
+/*
+ * platform_uart.c
+ *
+ *  Created on: Mar 16, 2026
+ *      Author: anirudhr
+ */
+#include "platform_uart.h"
+#include "main.h"
+#include <string.h>
+
+extern UART_HandleTypeDef huart2;
+
+void platform_uart_init(void)
+{
+	/* HAL already initialized UART in main.c */
+}
+
+void platform_uart_send(const char *msg)
+{
+	uint16_t len = strlen(msg);
+
+	HAL_UART_Transmit(&huart2,
+					  (uint8_t*)msg,
+					  len,
+					  HAL_MAX_DELAY);
+}
+
 
