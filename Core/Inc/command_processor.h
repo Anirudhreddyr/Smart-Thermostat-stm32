adbc50fbd6e0c09c9ca5506c20c6d9319812a1f5
Anirudh Reddy R <anirudhreddy1710@gmail.com>
2026-03-28 16:33:44 +0530

    Add command_processor.h header file

---
+++ Core/Inc/command_processor.h
@@ -0,0 +1,14 @@
+/*
+ * command_processor.h
+ *
+ *  Created on: Mar 16, 2026
+ *      Author: anirudhr
+ */
+
+#ifndef COMMAND_PROCESSOR_H
+#define COMMAND_PROCESSOR_H
+
+void command_processor_init(void);
+void command_processor_process(char *cmd);
+
+#endif /* COMMAND_PROCESSOR_H */
