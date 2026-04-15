3a75123df9d386feeedafce26ed0efa64caf45e4
Anirudh Reddy R <anirudhreddy1710@gmail.com>
2026-04-15 23:37:19 +0530

    Create thermostat_controller.h with init and event functions
    Add header file for thermostat controller functionality.

--- Core/Inc/thermostat_controller.h
+++ Core/Inc/thermostat_controller.h
@@ -1,1 +1,14 @@
+/*
+ * thermostat_controller.h
+ *
+ *  Created on: Mar 16, 2026
+ *      Author: anirudhr
+ */
 
+#ifndef THERMOSTAT_CONTROLLER_H
+#define THERMOSTAT_CONTROLLER_H
+
+void thermostat_controller_init(void);
+void thermostat_controller_process_event(void);
+
+#endif /* THERMOSTAT_CONTROLLER */
