e4b076a16346355ca406568355436ec9f2882be6
Anirudh Reddy R <anirudhreddy1710@gmail.com>
2026-04-29 21:51:17 +0530

    Add header file for temperature sensor functions

--- Core/Inc/temp_sensor.h
+++ Core/Inc/temp_sensor.h
@@ -1,1 +1,22 @@
+/*
+ * temp_sensor.h
+ *
+ *  Created on: Apr 28, 2026
+ *      Author: anirudhr
+ */
 
+#ifndef TEMP_SENSOR
+#define TEMP_SENSOR
+
+#include <stdint.h>
+
+/* Initialize sensor */
+void temp_sensor_init(void);
+
+/* Read temperature value */
+int temp_sensor_read(void);
+
+/* Scheduler task for sensor update */
+void temp_sensor_task(void);
+
+#endif /* TEMP_SENSOR */
