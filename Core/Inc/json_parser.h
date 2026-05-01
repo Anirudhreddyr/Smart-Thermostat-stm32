d259b1cc8f9a0612fd4e2a08d200103b407f2183
Anirudh Reddy R <anirudhreddy1710@gmail.com>
2026-05-01 20:18:03 +0530

    Add json_parser.h header file for JSON parsing

---
+++ Core/Inc/json_parser.h
@@ -0,0 +1,13 @@
+/*
+ * json_parser.h
+ *
+ *  Created on: Mar 16, 2026
+ *      Author: anirudhr
+ */
+
+#ifndef JSON_PARSER_H
+#define JSON_PARSER_H
+
+void json_parse(char *json);
+
+#endif /* JSON_PARSER_H */
