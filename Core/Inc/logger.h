3427228c4e358169fb8d5a0b788620b60591514e
Anirudh Reddy R <anirudhreddy1710@gmail.com>
2026-05-01 20:12:42 +0530

    Add logger header file with logging functionality

--- Core/Inc/logger.h
+++ Core/Inc/logger.h
@@ -1,1 +1,29 @@
+/*
+ * logger.h
+ *
+ *  Created on: Apr 29, 2026
+ *      Author: anirudhr
+ */
 
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdint.h>
+
+/* Log Levels */
+typedef enum
+{
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_DEBUG
+} log_level_t;
+
+/* Main logging function */
+void log_print(log_level_t level, const char *format, ...);
+
+/* Macros for easy usage */
+#define LOG_INFO(...)   log_print(LOG_LEVEL_INFO, __VA_ARGS__)
+#define LOG_ERROR(...)  log_print(LOG_LEVEL_ERROR, __VA_ARGS__)
+#define LOG_DEBUG(...)  log_print(LOG_LEVEL_DEBUG, __VA_ARGS__)
+
+#endif /* LOGGER_H */
